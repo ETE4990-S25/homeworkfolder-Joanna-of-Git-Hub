fb409a85cce33247cd59b7c427e107d735a7e29a
Joanna Escobar <kn6rpb@gmail.com>
2025-02-07 20:53:25 -0800

    got rid of for loop

--- Labs/Lab 3/NameList.py
+++ Labs/Lab 3/NameList.py
@@ -2,8 +2,8 @@ def nameInList(name, list):
     """Input a name and a list to seach through. 
     Returns if name was found"""
 
-    for items in list:
-        if name in list[items]:
-            print(name + "was in the list.")
-        else:
-            print(name + "was not in the list.")
+
+    if name in list:
+        print(name + "was in the list.")
+    else:
+        print(name + "was not in the list.")

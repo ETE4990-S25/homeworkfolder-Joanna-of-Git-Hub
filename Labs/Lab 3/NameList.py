4b0ce790f754d3d618a2cc3585a5a48e3570a5b8
Joanna Escobar <kn6rpb@gmail.com>
2025-02-07 20:43:53 -0800

    version 1 of list script

--- Labs/Lab 3/NameList.py
+++ Labs/Lab 3/NameList.py
@@ -1,2 +1,9 @@
-def nameInList():
-    
+def nameInList(name, list):
+    """Input a name and a list to seach through. 
+    Returns if name was found"""
+
+    for items in list:
+        if name == list[items]:
+            print(name + "was in the list.")
+        else:
+            print(name + "was not in the list.")

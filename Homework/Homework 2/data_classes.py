76dd211b9fafc6914769659104f83ec281045820
Joanna Escobar <kn6rpb@gmail.com>
2025-02-17 21:47:03 -0800

    creating Person and Student classes

--- Homework/Homework 2/data_classes.py
+++ Homework/Homework 2/data_classes.py
@@ -1,1 +1,13 @@
+class Person():
+    """A person in general."""
+    def __init__(self,name,age,email):
+        """Initialize personal details."""
+        self.name = name
+        self.age = age
+        self.email = email
+
+class Student(Person):
+    """A student."""
+    def __init__(self,student_id):
+        "Initializing student details."
 

f903b2a5ab0973f28f79b6e8302228072b95775a
Joanna Escobar <kn6rpb@gmail.com>
2025-02-24 18:09:16 -0800

    added info

--- Homework/Homework 3/data_classes.py
+++ Homework/Homework 3/data_classes.py
@@ -3,30 +3,30 @@ import json
 class Person(object):
     """A person in general."""
     
-    person_info={}
+    person_info={} #outside the definitions because I want it to be used throughout both classes
     person_file = 'personfile.json'
     
-    def __init__(self, name, age, email):
+    def __init__(self, name, age, email): #initializes basic info
         """Initialize personal details."""
         self.name = name
         self.age = age
         self.email = email
-    
-    def save_file(self):
-        """Take info from Person and put in json file."""
-        
-        person_info = {
+
+        self.person_info = { #stores all of the gathered data into a simple dictionary
             "name": self.name,
             "age": self.age,
             "email": self.email
         }
+    
+    def save_file(self):
+        """Take info from Person and put in json file."""     
         
-        with open(self.person_file, 'a') as f:
-            json.dump(person_info, f)
+        with open(self.person_file, 'w') as f: #using 'a' appends data instead of overwriting existing data
+            json.dump(self.person_info, f)
 
     def show_file(self):
-        filename = 'personfile.json'
-        with open(filename) as f:
+        self.person_file = 'personfile.json'
+        with open(self.person_file) as f:
             person_json = json.load(f)
 
         print(person_json)
@@ -38,7 +38,7 @@ class Student(Person):
         super().__init__(name, age, email)
 
         self.student_id = input("Input your ID number: ")
-        self.person_info.update({"student id":self.student_id})
+        self.person_info.update({"student id":self.student_id}) # I want to append student_id to the file
         
         #with open(self.student_id, 'w') as g:
             #json.dump(self.student_id, g)

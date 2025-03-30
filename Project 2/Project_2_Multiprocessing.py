0c1b9b6bd7e9a88b1b1e30bd00a987da8dc506b0
Joanna Escobar <kn6rpb@gmail.com>
2025-03-30 16:10:06 -0700

    added Manager

--- Project 2/Project_2_Multiprocessing.py
+++ Project 2/Project_2_Multiprocessing.py
@@ -11,9 +11,10 @@ def is_prime(n):
 
 
 if __name__ == "__main__":
-    # processes = []
-    # i = 0
-    rangec = range(0,100001)
+    processes = []
+    #manager = multiprocessing.Manager()
+    shared_dict = manager.dict()
+    i = 0
     
     #locking mechanism taken from ChatGPT
     largest_prime = multiprocessing.Value('i', 0)  
@@ -22,13 +23,16 @@ if __name__ == "__main__":
     end_time = time.time() + (3*60) # setting the end time to 3 minutes after starting
     print(f"End Time: {end_time}")
 
-    with multiprocessing.Pool(10) as p:
-        while time.time() < end_time:
-            p.map(is_prime, rangec) #args section from ChatGPT
-            # processes.append(p)        
+    while time.time() < end_time:
+        print(i)
+        p = multiprocessing.Process(target=is_prime, args=(i,largest_prime, lock)) #args section from ChatGPT
+        processes.append(p)        
+        p.start()
 
-            
-            # i = i + 1
+        
+        i = i + 1
 
+    for p in processes: 
+        p.join()
 
     print(f"Largest Prime: {largest_prime.value}")

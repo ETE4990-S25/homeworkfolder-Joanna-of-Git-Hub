62371f7c28b9a60aae585b644ffb0a5718194454
Joanna Escobar <kn6rpb@gmail.com>
2025-03-30 16:17:51 -0700

    added pipe, changed import values

--- Project 2/Project_2_Multiprocessing.py
+++ Project 2/Project_2_Multiprocessing.py
@@ -1,4 +1,4 @@
-import multiprocessing
+from multiprocessing import Process, Pipe, Lock, Value, Manager
 import time
 
 def is_prime(n):
@@ -12,20 +12,22 @@ def is_prime(n):
 
 if __name__ == "__main__":
     processes = []
-    #manager = multiprocessing.Manager()
-    shared_dict = manager.dict()
-    i = 0
-    
+    NUM_OF_PROCESSES = 10
+    num_to_factor = 10000000 #very large number to (hopefully get to by the end of 3 minutes)
+    range_per_process = num_to_factor // NUM_OF_PROCESSES
+
+    parent_conns, child_conns = zip(*[Pipe() for _ in range(NUM_OF_PROCESSES)])
+
     #locking mechanism taken from ChatGPT
-    largest_prime = multiprocessing.Value('i', 0)  
-    lock = multiprocessing.Lock()  
+    largest_prime = Value('i', 0)  
+    lock = Lock()  
  
     end_time = time.time() + (3*60) # setting the end time to 3 minutes after starting
     print(f"End Time: {end_time}")
 
     while time.time() < end_time:
         print(i)
-        p = multiprocessing.Process(target=is_prime, args=(i,largest_prime, lock)) #args section from ChatGPT
+        p = Process(target=is_prime, args=(i,largest_prime, lock)) #args section from ChatGPT
         processes.append(p)        
         p.start()
 

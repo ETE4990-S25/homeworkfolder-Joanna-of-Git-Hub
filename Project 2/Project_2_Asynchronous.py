71fb9c918188ea28a340a55da6280081bcc26099
Joanna Escobar <kn6rpb@gmail.com>
2025-04-04 22:59:49 -0700

    changed Asynch to take multiprocessing result

--- Project 2/Project_2_Asynchronous.py
+++ Project 2/Project_2_Asynchronous.py
@@ -2,7 +2,7 @@ import time
 import asyncio 
 import nest_asyncio # from Mr. Power's notes: this will fix the iPython unable to perform Asynchronous tasks
 nest_asyncio.apply()
-import Project_2_Threading
+import Project_2_Multiprocessing
 
 async def factorial(n):
     if n == 0:
@@ -14,12 +14,12 @@ async def factorial(n):
 
 # taken from Mr. Power's notes
 num = 0
-fibonnaci_result = Project_2_Threading.fibonnaci_number
+n = Project_2_Multiprocessing.largest_prime.value
 factorial_number = 0
 
 async def main():
     if time.time() < asy_end: 
-        await factorial_number = asyncio.gather(*(factorial(num) for num in fibonnaci_result))
+        await factorial_number = asyncio.gather(*(factorial(num) for num in n))
         # if num > factorial_result:
         #     factorial_result = num
         

e431f1d1c74b04648e39560940a25f5e5544eb60
Joanna Escobar <kn6rpb@gmail.com>
2025-04-12 00:04:00 -0700

    Completed Multiprocessing

--- Project 2/Project_2_Multiprocessing.py
+++ Project 2/Project_2_Multiprocessing.py
@@ -1,7 +1,11 @@
 from multiprocessing import Process, Pipe, Lock, Value, Manager, Queue, Pool
 import time
 
-def is_prime(start, end):
+NUM_OF_PROCESSES = 4
+lock = Lock()
+largest_prime = Value('i',0)
+
+def is_prime(largest_prime, start, end):
     for a in range(start,end):
         if a <= 1:
             return False
@@ -10,46 +14,24 @@ def is_prime(start, end):
                 return False
         largest_prime.value = a
 
-NUM_OF_PROCESSES = 4
-lock = Lock()
-largest_prime = Value('i',0)
-
 if __name__ == "__main__":
-    end_time = time.time() + 30 #180 # setting the end time to 3 minutes after starting
+    end_time = time.time() + 180 # setting the end time to 3 minutes after starting
     print(f"End Time: {end_time}")
     
     start = 1
-
-    if time.time() < end_time:
+    
+    
+    while time.time() < end_time:
         
-        end = start + 12
-        start = end + 1
+        end = start + 60000
 
-        processes = [Process(target=is_prime, args=(start,end)) for i in range(NUM_OF_PROCESSES)]
+        processes = [Process(target=is_prime, args=(largest_prime,start,end)) for i in range(NUM_OF_PROCESSES)]
     
         for p in processes:
             p.start()
         for p in processes: 
             p.join()
+        
+        start = end
  
     print(f"Largest Prime: {largest_prime.value}")
-
-
-
-    # i = 0
-
-    # while time.time() < end_time and i < NUM_OF_PROCESSES:
-    #     print(i)
-    #     #p = Process(target=is_prime, args=(i * range_per_process + 1, (i + 1) * range_per_process + 1))
-    #     p = [Process(target=is_prime, args=()) for i in range(NUM_OF_PROCESSES)]
-    #     #processes.append(p)        
-    #     for processes in p:
-    #         processes.start()
-    #     for processes in p: 
-    #         processes.join()
-    #     #i = i + 1
-
-
-    
-    
-

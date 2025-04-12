f8d5382bda4d87f9ebe1e3a5f00193b2f1620acd
Joanna Escobar <kn6rpb@gmail.com>
2025-04-11 20:44:34 -0700

    added code to log_part_1

--- Labs/log_part_1.py
+++ Labs/log_part_1.py
@@ -1,4 +1,23 @@
 import logging
-import datetime
+import logging.handlers
+from datetime import timedelta, datetime
+import time
+import freezegun
 
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
+parent1 = logging.getLogger("logger_parent1")
 
+parent1.log(logging.CRITICAL, "No more disk space")
+
+parent1.critical("Computer Angry")
+parent1.error("file not found in directory")
+parent1.warning("computer overheating")
+parent1.info("You have turned on the computer")
+
+parent1.addHandler(
+    logging.handlers.TimedRotatingFileHandler(
+        filename = "parent1_archived_log.log",
+        when = "D",
+        backupCount = 3,
+    )
+)

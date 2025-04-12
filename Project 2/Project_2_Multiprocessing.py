6c73682036bdb9bb7647a0be0124cc88ebd4285f
Joanna Escobar <kn6rpb@gmail.com>
2025-04-12 00:56:30 -0700

    edited Threading

--- Project 2/Project_2_Multiprocessing.py
+++ Project 2/Project_2_Multiprocessing.py
@@ -6,9 +6,10 @@ import nest_asyncio # from Mr. Power's notes: this will fix the iPython unable t
 nest_asyncio.apply()
 import time
 
+end_time = 0
+
 ### Project_2_Multiprocessing.py ###
 NUM_OF_PROCESSES = 4
-lock = Lock()
 largest_prime = Value('i',0)
 
 def is_prime(largest_prime, start, end):
@@ -24,13 +25,9 @@ if __name__ == "__main__":
     end_time = time.time() + 180 # setting the end time to 3 minutes after starting
     print(f"End Time: {end_time}")
     
-    start = 1
-    
-    
-    while time.time() < end_time:
-        
+    start = 0
+    while time.time() < end_time: 
         end = start + 60000
-
         processes = [Process(target=is_prime, args=(largest_prime,start,end)) for i in range(NUM_OF_PROCESSES)]
     
         for p in processes:
@@ -38,44 +35,39 @@ if __name__ == "__main__":
         for p in processes: 
             p.join()
         
-        start = end
+        start = end + 1
  
     print(f"Largest Prime: {largest_prime.value}")
 
 ### Project_2_Threading.py ###
-n = largest_prime.value
-THREAD_POOL_SIZE = 10
-fibonnaci_number = 0 
+target_num = largest_prime.value
 lock = Lock()
+THREAD_POOL_SIZE = 10
+fibonnaci_number = Value('i',0)
 
-def fibonnaci(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else: 
-        return fibonnaci(n - 1) + fibonnaci(n - 2)
+def fibonnaci(a):
+    with lock: 
+        if a == 0:
+            return 0
+        elif a == 1:
+            return 1
+        else: 
+            return fibonnaci(a - 1) + fibonnaci(a - 2)
 
 def worker(work_queue): # Taken from Mr. Power's lecture notes.
-    global fibonnaci_number
     while not work_queue.empty():
         try:
             item = work_queue.get(block=False)
         except Empty:
             break
         else:
-            # taken lock structure from ChatGPT
-            if fibonnaci(item):
-                with lock: 
-                    if item > fibonnaci_number:
-                        fibonnaci_number = item
+            fibonnaci(target_num)
             work_queue.task_done()
 
 def threaded_pool(): # Taken from Mr. Power's lecture notes.           
-    global fibonnaci_number
     work_queue = Queue()
     
-    for i in range(0,n): # changed the n to create the largest number the thing will (hopefully get to)
+    for i in range(0, target_num + 1): 
         work_queue.put(i)
         threads = [
             Thread(target=worker, args = (work_queue,)) for _ in range(THREAD_POOL_SIZE)
@@ -89,9 +81,8 @@ def threaded_pool(): # Taken from Mr. Power's lecture notes.
     while threads:    #used to delay the time output lines
         threads.pop().join()
 
-
-
-threaded_pool()
+if time.time >= end_time:
+    threaded_pool()
 
 print(f"Fibonnaci number: {fibonnaci_number}")
 
@@ -112,12 +103,11 @@ n = largest_prime.value
 factorial_number = 0
 
 async def main():
-    if time.time() < asy_end: 
-        await factorial_number = asyncio.gather(*(factorial(num) for num in n))
-        # if num > factorial_result:
-        #     factorial_result = num
-        
-    else: 
-        print(f"Factorial Result: {factorial_number}") 
+    await factorial_number == asyncio.gather(*(factorial(num) for num in n))
+    # if num > factorial_result:
+    #     factorial_result = num
+    
+
+    print(f"Factorial Result: {factorial_number}") 
 
 asyncio.run(main())

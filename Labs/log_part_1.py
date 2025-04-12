2f2bcd1478761135d4a61c641910715ff1361fd3
Joanna Escobar <kn6rpb@gmail.com>
2025-04-11 20:53:37 -0700

    added info

--- Labs/log_part_1.py
+++ Labs/log_part_1.py
@@ -4,15 +4,15 @@ from datetime import timedelta, datetime
 import time
 import freezegun
 
+# configurations for parent1
 logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
 parent1 = logging.getLogger("logger_parent1")
-
-parent1.log(logging.CRITICAL, "No more disk space")
-
-parent1.critical("Computer Angry")
-parent1.error("file not found in directory")
-parent1.warning("computer overheating")
-parent1.info("You have turned on the computer")
+parent1.setLevel(logging.INFO)
+formatting = logging.Formatter(
+    fmt = ("%(asctime)s | %(levelname)s"
+           "%(message)s"
+    )
+)
 
 parent1.addHandler(
     logging.handlers.TimedRotatingFileHandler(
@@ -21,3 +21,13 @@ parent1.addHandler(
         backupCount = 3,
     )
 )
+
+
+# These guys print the level and error name on the terminal
+# parent1.log(logging.CRITICAL, "No more disk space")
+
+# parent1.critical("Computer Angry")
+# parent1.error("file not found in directory")
+# parent1.warning("computer overheating")
+# parent1.info("You have turned on the computer")
+

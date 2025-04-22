03cb856e1497b3fbf79ed9f718209813274b520d
Joanna Escobar <kn6rpb@gmail.com>
2025-04-21 19:48:45 -0700

    two functions made for part 2

--- Labs/Lab 9/log_part_2.py
+++ Labs/Lab 9/log_part_2.py
@@ -1,2 +1,11 @@
 import re
 import json
+
+def read_log_line(log):
+    # structure taken from Mr. Power's Lecture 6 notes
+    with open(log, "r") as file:
+        lines = file.readlines()
+
+def parse_log_line(line):
+    pattern = r"^(.*?)\s\|\s(\w+)\s\|\s(\w+)\s\|\s(.*)$"
+    match = re.match(pattern, line)

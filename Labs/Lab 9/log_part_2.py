cd7c7d0c6c37f4ec52284a59d12de02deb625c3f
Joanna Escobar <kn6rpb@gmail.com>
2025-04-21 20:07:10 -0700

    edited log part 2

--- Labs/Lab 9/log_part_2.py
+++ Labs/Lab 9/log_part_2.py
@@ -1,10 +1,10 @@
 import re
-import json
+import ast
 
-def read_log_line(log):
+def read_log_line(log_filepath):
     # structure taken from Mr. Power's Lecture 6 notes
-    with open(log, "r") as file:
-        lines = file.readlines()
+    with open(log_filepath, "r") as file:
+        lines = ast.literal_eval(file.readlines())
 
 def parse_log_line(line):
     pattern = r"^(.*?)\s\|\s(\w+)\s\|\s(\w+)\s\|\s(.*)$"

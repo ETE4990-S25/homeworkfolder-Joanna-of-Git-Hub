ed17e45b8b442288e426b1ccad536afe3424148f
Joanna Escobar <kn6rpb@gmail.com>
2025-04-22 23:39:58 -0700

    edited part 2

--- Labs/Lab 9/log_part_2.py
+++ Labs/Lab 9/log_part_2.py
@@ -11,15 +11,30 @@ def read_log_line(log_filepath):
         FileNotFoundError
 
 def parse_log_line(line_list):
+    match_list = []
+    
     for line in line_list:
         # print(f"{line}") # it can go thru all lines. line var considered a string
 
         pattern = r"^(.*?)\s\|\s(\w+)\s\|\s(\w+)\s\|\s(.*)$"
         match = re.match(pattern, line) #this tries to match each entire line
         
-        if match: #this section modified from ChatGPT
-           return match.groups()
+        if match: #this part modified from ChatGPT
+           match_list.append(match.groups()) 
 
+def count_log_levels(parsed):
+    count_dict = {"INFO": {}, "WARNING":{}, "ERROR":{}, "CRITICAL":{}}
 
-def count_log_levels(line):
-    a = 1
+    for tuple in parsed:
+        level = tuple[2]
+        message = tuple[3]
+        count_i, count_e, count_w, count_l = 0
+
+        if level == "INFO":
+            count_dict["INFO"][message] = count_i + 1
+        elif level == "ERROR":
+            count_dict["ERROR"][message] = count_e + 1
+        elif level == "WARNING":
+            count_dict["WARNING"][message] = count_w + 1
+        elif level == "CRITICAL":
+            count_dict["CRITICAL"][message] = count_l + 1

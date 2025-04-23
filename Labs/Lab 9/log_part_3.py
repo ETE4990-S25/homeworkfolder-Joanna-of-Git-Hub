84baa822e600460da5ac1a9e9f53e8df80557012
Joanna Escobar <kn6rpb@gmail.com>
2025-04-23 16:07:30 -0700

    finished log 3 without daemons

--- Labs/Lab 9/log_part_3.py
+++ Labs/Lab 9/log_part_3.py
@@ -1,4 +1,3 @@
-import json
 import matplotlib
 
 def jsonMonitor(file):
@@ -19,5 +18,35 @@ def jsonMonitor(file):
             for value in key:
                 print(count_dict[key][value])
 
-def graphErrors(input):
-    a =1
+def graphErrors(file):
+    """Takes in a json or txt file that has already parsed the amount and type of errors."""
+    
+    with open(file, "r") as f:
+        count_dict = f.load()
+
+    #taken from Mr. Powers's notes
+    categories = list("INFO","ERROR","WARNING","CRITICAL")
+    
+    count_i, count_e, count_w, count_c = 0
+    count_list = []
+    for key in count_dict:
+        if key == "INFO":
+            count_i += 1
+        elif key == "ERROR":
+            count_w += 1
+        elif key == "WARNING":
+            count_e += 1
+        else:
+            count_c += 1
+
+    count_list.append(count_i)
+    count_list.append(count_e)
+    count_list.append(count_w)
+    count_list.append(count_c)
+
+    matplotlib.bar(categories, count_list)
+    matplotlib.title('Logged Information')
+    matplotlib.xlabel('Categories')
+    matplotlib.ylabel('Amount')
+
+    matplotlib.show()

1f66224b96c358980668f92d359b99daf05fa77b
Joanna Escobar <kn6rpb@gmail.com>
2025-04-24 16:20:34 -0700

    edited lab 9

--- Labs/Lab 9/log_part_2.py
+++ Labs/Lab 9/log_part_2.py
@@ -1,3 +1,4 @@
+# Paste your python file here don't for get to upload it with your submission
 import re
 
 def read_log_line(log_filepath):
@@ -9,17 +10,17 @@ def read_log_line(log_filepath):
     except:
         FileNotFoundError
 
+
 def parse_log_line(line_list):
     match_list = []
-    
+
     for line in line_list:
         # print(f"{line}") # it can go thru all lines. line var considered a string
-
         pattern = r"^(.*?)\s\|\s(\w+)\s\|\s(\w+)\s\|\s(.*)$"
-        match = re.match(pattern, line) #this tries to match each entire line
-        
-        if match: #this part modified from ChatGPT
-           match_list.append(match.groups()) 
+        match = re.match(pattern, line) # this tries to match entire line
+
+        if match: # this part modified from ChatGPT
+            match_list.append(match.groups())
 
 def count_log_levels(parsed):
     count_dict = {"INFO": {}, "WARNING":{}, "ERROR":{}, "CRITICAL":{}}
@@ -27,7 +28,7 @@ def count_log_levels(parsed):
     for tuple in parsed:
         level = tuple[2]
         message = tuple[3]
-        count_i, count_e, count_w, count_l = 0
+        count_i, count_e, count_w, count_c = 0
 
         if level == "INFO":
             count_dict["INFO"][message] = count_i + 1
@@ -36,7 +37,7 @@ def count_log_levels(parsed):
         elif level == "WARNING":
             count_dict["WARNING"][message] = count_w + 1
         elif level == "CRITICAL":
-            count_dict["CRITICAL"][message] = count_l + 1
+            count_dict["CRITICAL"][message] = count_c + 1
 
     filename = 'log_level_count.json'
     with open(filename,'w') as f:

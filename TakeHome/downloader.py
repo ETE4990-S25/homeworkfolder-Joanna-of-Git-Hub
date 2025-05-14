9429d00a81b1a2b5112719b77c23f687e10196b0
Joanna Escobar <kn6rpb@gmail.com>
2025-05-13 22:20:52 -0700

    created increment_data file and updated notebook

--- TakeHome/downloader.py
+++ TakeHome/downloader.py
@@ -3,47 +3,16 @@ import xmltodict
 import json
 import random
 import threading
-import time
-import parser
+import parser, increment_date # importing my own files
 
 rates = ["EUR", "GBP", "USD", "DZD", "AUD", "BWP", "BND", "CAD", "CLP", "CNY", "COP", "CZK", "DKK", "HUF", "ISK", "INR", "IDR", "ILS", "KZT", "KRW", "KWD", "LYD", "MYR", "MUR", "NPR", "NZD", "NOK", "OMR", "PKR", "PLN", "QAR", "RUB", "SAR", "SGD", "ZAR", "LKR", "SEK", "CHF", "THB", "TTD"]
 ratesForBase = [r for r in rates if r != "USD" and r != "EUR" and r != "GBP"]
 
-i_year = 2011
-i_month = 5
-i_day = 3
-days_31 = [1,3,5,7,8,10,12]
-days_30 = [4,6,9,11]
-
-today_string = time.strftime("%Y-%m-%d",time.gmtime()) # figured this out using the Python datasheet
+# i_year = 2011
+# i_month = 5
+# i_day = 4
 
 base = random.choice(ratesForBase)
-
-def increment_date(year, month, day):
-    """A function to increment the date and returns date in yr-mos-day format as a string."""
-    # incrementing dates depending on the month
-    if day == 31 and month == 12:
-        year += 1
-    elif day == 31 and month in days_31:
-        month += 1
-    elif day == 30 and month in days_30:
-        month += 1
-    elif day == 28 and month == 2 or day == 29 and month == 2:
-        month += 1
-    else:
-        day += 1
-
-    # turning date into a string. Adds a zero ahead of day/month number if needed.
-    if day < 10 and month > 10:
-        date = f"{year}-{month}-0{day}"
-    elif day < 10 and month < 10:
-        date = f"{year}-0{month}-0{day}"
-    elif day > 10 and month < 10:
-        date = f"{year}-0{month}-{day}"
-    else:
-        date = f"{year}-{month}-{day}"
-    
-    return date
 
 def get_data(date, base):
     # URL of thetData data
@@ -61,7 +30,8 @@ def get_data(date, base):
     # Convert the dictionary to a JSON string
     return json.dumps(data_dict, indent=4)
 
-
+while date != today_string:
+    date = 1
 
 
 
---
+++ TakeHome/increment_date.py
@@ -0,0 +1,54 @@
+import time
+
+today_str = time.strftime("%Y-%m-%d",time.gmtime()) # figured this out using the Python datasheet
+
+# for keeping track of which months have 30 or 31 days
+days_31 = [1,3,5,7,8,10,12]
+days_30 = [4,6,9,11]
+
+class Date(object):
+    """Keeps track of year, month, day while allowing values to be iterated."""
+    
+    def __init__(self, year=2011, month=5, day=4):
+        """Initializes values, defaulting to our starting day 2011-05-04."""
+        self.year = year
+        self.month = month
+        self.day = day
+
+    def increment_date(self):
+        """A function to increment the date and returns date in yr-mos-day format as a string."""
+        # incrementing dates depending on the month
+        if self.month == 12 and self.day == 31:
+            self.year += 1
+            self.month = 1
+            self.day = 1
+        
+        elif self.day == 31 and self.month in days_31:
+            self.month += 1
+            self.day = 1
+        
+        elif self.day == 30 and self.month in days_30:
+            self.month += 1
+            self.day = 1
+        
+        elif self.month == 2:
+            if self.day == 29 or self.day == 28:
+                self.month += 1
+                self.day = 1
+        
+        else:
+            self.day += 1
+
+        # turning date into a string. Adds a zero ahead of day/month number if needed.
+        if self.day < 10 and self.month > 10:
+            date_str = f"{self.year}-{self.month}-0{self.day}"
+        elif self.day < 10 and self.month < 10:
+            date_str = f"{self.year}-0{self.month}-0{self.day}"
+        elif self.day > 10 and self.month < 10:
+            date_str = f"{self.year}-0{self.month}-{self.day}"
+        else:
+            date_str = f"{self.year}-{self.month}-{self.day}"
+        
+        return date_str
+    
+date = Date()

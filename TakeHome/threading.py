8553a41b12907d3430f2d9507cba6db75d8bb67e
Joanna Escobar <kn6rpb@gmail.com>
2025-05-15 00:01:16 -0700

    updated threading file

---
+++ TakeHome/threading.py
@@ -0,0 +1,44 @@
+import threading
+from queue import Queue, Empty
+import time
+import json
+from increment_date import Date, today_str # importing my own files
+from downloader import get_data, date_str, base, ratesForBase, exc_dict
+    
+def worker(work_queue): # taken from Mr. Power's notes
+    while not work_queue.empty():
+        try:
+            item = work_queue.get(block=False)
+        except Empty:
+            break
+        else:
+            get_data(date_str,base)
+            work_queue.task_done()
+
+def threaded_pool(): # taken from Mr. Power's notes           
+    work_queue = Queue()
+
+    for base in ratesForBase:
+        work_queue.put(base)
+        threads = [
+            threading.Thread(target=worker, args=(work_queue,)) 
+            for _ in range(5)
+        ]
+    
+    for thread in threads:
+        thread.start()
+
+    work_queue.join()
+
+    while threads:    #used to delay the time output lines
+        threads.pop().join
+
+threaded_pool()
+
+# while date_str <= today_str:
+#     date.increment_date() # increments the yr/mo/day values in the class Date
+
+# Write the new dictionary to a JSON file
+exc_dict_json = json.dumps(exc_dict, indent=4)
+with open(f"{base}_exchange_rates.json", "w") as rate_dict:
+    rate_dict.write(exc_dict_json)
